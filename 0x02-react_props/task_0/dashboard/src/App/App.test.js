6287e58d154dab31ada18344cea1fac13b42eddc
Warmarth <godsonibeaka@gmail.com>
2023-05-23 00:14:09 +0100

    done

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -1,7 +1,18 @@
+import React from "react";
 import App from "./App";
+import { shallow } from "enzyme";
 
-test("renders learn react link", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+describe("App tests", () => {
+    it("renders without crashing", () => {
+        const component = shallow(<App />);
+
+        expect(component).toBeDefined();
+    });
+
+// import App from "./App";
+
+// test("renders learn react link", () => {
+//     render(<App />);
+//     const linkElement = screen.getByText(/learn react/i);
+//     expect(linkElement).toBeInTheDocument();
 });
