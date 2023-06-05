407c3402bc440d86e6797e19a2d2435743b3c455
Warmarth <godsonibeaka@gmail.com>
2023-06-05 18:54:22 +0100

    done

--- 0x01-react_intro/task_4/dashboard/src/Notifications.test.js
+++ 0x01-react_intro/task_4/dashboard/src/Notifications.test.js
@@ -6,13 +6,15 @@ describe("Notifications", () => {
   it("renders without crashing", () => {
     shallow(<Notifications />);
   });
+
   it("should render three list items", () => {
     const wrapper = shallow(<Notifications />);
     wrapper.update();
     expect(wrapper.find("li")).toHaveLength(3);
   });
+
   it('should render text "Here is the list of notifications" ', () => {
     const wrapper = shallow(<Notifications />);
-    expect(wrapper.find("p").text()).toBe("Here is the list of notifications")
+    expect(wrapper.find("p").text()).toBe("Here is the list of notifications");
   });
 });
