8e7e41ce7e3c1234585e3f8005fd7b42826206ac
Warmarth <godsonibeaka@gmail.com>
2023-06-16 01:44:42 +0100

    done

--- 0x03-React_component/task_0/dashboard/src/index.js
+++ 0x03-React_component/task_0/dashboard/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App/App";
 import Notifications from "./Notifications/Notifications";
 
 ReactDOM.render(
   <React.StrictMode>
     <div id="root-notifications">
-      <Notifications />
+    <Notifications />
     </div>
     <App />
   </React.StrictMode>,
